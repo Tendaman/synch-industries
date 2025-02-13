12159096683e24a1d565830aa35e82eef7e9e4af
Tendaman <122923185+Tendaman@users.noreply.github.com>
2025-02-13 13:50:38 +0200

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 import "./src/lib/delExpCodes"; // Import the cleanup job
 import "./src/lib/dupDataToHistory"
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Enable React strict mode
   swcMinify: true, // Use SWC for faster minification
